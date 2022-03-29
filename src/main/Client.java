23e2fa0be2bfce1ab7bcfa2286fba027c84950c6
Alban Ktz <alban.ktz@gmail.com>
2022-03-29 17:54:15 +0200

    add icone

--- src/main/Client.java
+++ src/main/Client.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ public class Client extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("../Vue/VueAccueil.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Jeux des Allumettes");
+        stage.getIcons().add(new Image("./Vue/images/logo.png"));
         stage.setScene(scene);
         stage.show();
     }
